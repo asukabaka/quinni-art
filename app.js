23ac71cc0710ea34e7a2bbe04adac924da1d65e0
asukabaka <48495833+asukabaka@users.noreply.github.com>
2020-11-19 18:59:33 -0800

    edit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 2,
         "density": {
           "enable": true,
           "value_area": 800
@@ -37,7 +37,7 @@ particlesJS('particles-js',
         "image": {
           "src": "cloud.png",
           "width": 100,
-          "height": 100
+          "height": 50
         }
       },
       "opacity": {
@@ -51,12 +51,12 @@ particlesJS('particles-js',
         }
       },
       "size": {
-        "value": 25,
-        "random": true,
+        "value": 170,
+        "random": false,
         "anim": {
           "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
+          "speed": 20,
+          "size_min": 10,
           "sync": false
         }
       },
@@ -69,7 +69,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 6,
+        "speed": .5,
         "direction": "none",
         "random": false,
         "straight": false,
